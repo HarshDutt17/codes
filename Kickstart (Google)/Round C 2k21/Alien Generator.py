6d2a7c14ed612d49622a9cd7ffc31fb8e8c5f83f
HarshDutt17 <78022802+HarshDutt17@users.noreply.github.com>
2021-05-29 16:53:06 +0530

    Update Alien Generator.py

--- Kickstart (Google)/Round C 2k21/Alien Generator.py
+++ Kickstart (Google)/Round C 2k21/Alien Generator.py
@@ -3,15 +3,14 @@ for tt  in range(1,T+1):
     G=int(input())
     a=1
     possible=0
-    while a*a <= G :
-        if G%a==0:
-            if a!=G//a:
-                if a%2==1:
-                    possible+=1
-                if (G//a)%2==1:
-                    possible+=1
-            else:
-                if a%2==1:
+    G2=2*G
+    while a*a <= G2 :
+        if G2%a==0:
+            b=G2//a
+            if a%2!=b%2:
+                n=(a+b-1)//2
+                k=(b-a-1)//2
+                if (n-k)*(n+k+1) == G2:
                     possible+=1
         a+=1
     print("Case #{}:".format(tt),possible)
